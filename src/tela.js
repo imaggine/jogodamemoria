1d4b2a9336e99ee401757495285beff996a54478
Marcos Guiimarães <marcos@imaggineapps.com>
2021-02-26 11:12:07 -0300

    alteração no tamanho da imagem

--- src/tela.js
+++ src/tela.js
@@ -22,7 +22,7 @@ class Tela {
     static obterCodigoHtml(item){
         return `
         <div class="col-md-3">
-            <div class="card" style="width: 50%;" onclick="window.verificarSelecao('${item.id}', '${item.nome}')">
+            <div class="card" style="width: 10%;" onclick="window.verificarSelecao('${item.id}', '${item.nome}')">
                 <img src="${item.img}" name="${item.nome}" id="${item.id}" class="card-img-top" alt="...">
             </div>   
             <br />     
